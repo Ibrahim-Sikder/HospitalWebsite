afcb158743afe7de0bb31a1d2526ff5b90fc1479
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-02-25 11:16:56 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+window.addEventListener('resize', changeSearchIcon);
+
+function changeSearchIcon(){
+    let winsize = window.matchMedia("(min-width:1200px)");
+    if(winsize.mathces){
+        document.querySelector('.search-icon-img').src = "images/search-icon.png";
+    }
+    else{
+        document.querySelector(".search-icon- img").src = "images/search-icon-dark.png";
+    }
+}
+changeSearchIcon();
+
+// stopping at animation and transition
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() =>{
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
